aec98bcdca69de93b5dc392ff7e424292844134f
watashiwaaniket <aniketdhakane3@gmail.com>
2025-03-02 19:56:09 +0530

    Changed styling a bit

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,10 @@ function App() {
     <ConnectionProvider endpoint={import.meta.env.VITE_ALCHEMY_DEVNET_RPC_URL}>
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
-          <div className='w-[560px] h-[600px] flex flex-col items-center justify-center bg-violet-400 rounded-3xl p-4 text-slate-900 border-2 border-orange-400'>
-            <div className='flex flex-col items-center justify-center bg-violet-600 rounded-2xl p-6 w-[400px] h-[280px] border-2 border-slate-900'>
-              <h2 className='font-bold text-3xl mb-8'>Solana Faucet</h2>
+          <div className='w-[560px] h-[600px] flex flex-col items-center justify-center bg-[#640D5F] rounded-3xl p-4 text-slate-900 border-2 border-orange-400'>
+            <div className='flex flex-col items-center justify-center bg-[#D91656] rounded-2xl p-6 w-[400px] h-[280px] border-2 border-slate-900'>
+              <h2 className='font-bold text-3xl'>Solana Faucet</h2>
+              <p className='text-[#FFB200] font-semibold mb-8'>Gift yourself some devnet solana!</p>
               <h2 className='font-semibold text-base'>Connect Wallet</h2>
               <WalletMultiButton />
             </div>
--- src/components/Airdrop.jsx
+++ src/components/Airdrop.jsx
@@ -26,9 +26,9 @@ export default function Airdrop() {
     }
 
     return(
-        <div className="p-6 flex flex-col items-center justify-center bg-violet-600 rounded-2xl mt-6 w-[400px] h-[200px] border-2 border-slate-900">
-            <input type="number" className="bg-orange-200 rounded-xl p-2 border-2 border-orange-400" value={amount} onChange={handleInputChange} placeholder="Enter the value" />
-            <button className="mt-2 w-[160px] bg-orange-400 border-2 border-violet-600 text-violet-600 rounded-lg font-bold" onClick={sendAirdrop}>Send Airdrop</button>
+        <div className="p-6 flex flex-col items-center justify-center bg-[#D91656] rounded-2xl mt-6 w-[400px] h-[200px] border-2 border-slate-900">
+            <input type="number" className="bg-orange-200 rounded-xl p-2 border-2 border-[#640D5F]" value={amount} onChange={handleInputChange} placeholder="Enter the value" />
+            <button className="mt-2 w-[160px] bg-orange-400 border-2 border-[#640D5F] text-[#640D5F] rounded-lg font-bold" onClick={sendAirdrop}>Send Airdrop</button>
         </div>
     )
 };
